a349d46ff8aedd3d4188877a4b79483fbb6b9616
OwenLu <gg0125gg@gmail.com>
2023-05-15 00:25:25 +0800

    feat: modify Form -Render page based on step

--- src/component/Main/Register/Form.jsx
+++ src/component/Main/Register/Form.jsx
@@ -1,15 +1,28 @@
-import { AddressPhase, CreditPhase, ShippingPhase } from './FormPhase'
-import styles from './Form.module.scss'
-const Form = () => {
+import { AddressPhase, ShippingPhase, CreditPhase } from './FormPhase';
+import styles from './Form.module.scss';
+
+const Form = ({ step }) => {
+  // 根據 step 的值來渲染相對應的組件
+  const renderPhase = () => {
+    switch (step) {
+      case 0:
+        return <AddressPhase />;
+      case 1:
+        return <ShippingPhase />;
+      case 2:
+        return <CreditPhase />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <>
       <section className={`${styles.formContainer} col col-12`}>
-        <AddressPhase />
-        {/* <ShippingPhase /> */}
-        {/* <CreditPhase /> */}
+        {renderPhase()} {/* 根據 step 渲染相應的組件 */}
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
